853152027f5d1f7267874cd454dc4c6d80918972
Tymoteusz Jankowski <tymekwj@hotmail.com>
2023-11-09 03:52:06 +0100

    Finished

--- Sorting/Form1.cs
+++ Sorting/Form1.cs
@@ -1,21 +1,16 @@
 using System.Diagnostics;
 
-namespace Sorting
-{
-    public partial class Form1 : Form
-    {
+namespace Sorting {
+    public partial class Form1 : Form {
         private Random rnd = new Random();
-        public Form1()
-        {
+        public Form1() {
             InitializeComponent();
         }
 
-        private List<int> parseInput()
-        {
-            string[] numS = inputTB.Text.Trim().Split(' ');
+        private List<int> parseInput() {
+            string[] numS = inputTB.Text.Trim().Split(null as char[], StringSplitOptions.RemoveEmptyEntries);
             List<int> numI = new List<int>(numS.Length);
-            foreach(string s in numS)
-            {
+            foreach(string s in numS) {
                 int x;
                 int.TryParse(s, out x);
                 numI.Add(x);
@@ -23,33 +18,26 @@ namespace Sorting
             return numI;
         }
 
-        private List<int> parseOrGenerate()
-        {
+        private List<int> parseOrGenerate() {
             List<int> numI;
-            if(!inputCB.Checked)
-            {
+            if(!inputCB.Checked) {
                 int n = (int)genNUD.Value;
                 numI = new List<int>(n);
-                for(int i = 0; i < n; i++)
-                {
+                for(int i = 0; i < n; i++) {
                     numI.Add(rnd.Next());
                 }
             }
-            else
-            {
+            else {
                 numI = parseInput();
             }
             return numI;
         }
 
-        private void printResult(List<int> result, long time)
-        {
+        private void printResult(List<int> result, long time) {
             outputL.Text = "";
             timeValL.Text = "";
-            if(inputCB.Checked)
-            {
-                foreach(int n in result)
-                {
+            if(inputCB.Checked) {
+                foreach(int n in result) {
                     outputL.Text += n;
                     outputL.Text += ' ';
                 }
@@ -57,8 +45,7 @@ namespace Sorting
             timeValL.Text = (time.ToString() + " ms");
         }
 
-        private void deactivateControls()
-        {
+        private void deactivateControls() {
             inputCB.Enabled = false;
             inputTB.Enabled = false;
             genNUD.Enabled = false;
@@ -68,39 +55,32 @@ namespace Sorting
             msB.Enabled = false;
             qsB.Enabled = false;
         }
-        private void activateControls()
-        {
+        private void activateControls() {
             inputCB.Enabled = true;
             bsB.Enabled = true;
             ssB.Enabled = true;
             isB.Enabled = true;
             msB.Enabled = true;
             qsB.Enabled = true;
-            if(inputCB.Checked == true)
-            {
+            if(inputCB.Checked == true) {
                 inputTB.Enabled = true;
                 genNUD.Enabled = false;
             }
-            else
-            {
+            else {
                 inputTB.Enabled = false;
                 genNUD.Enabled = true;
             }
         }
 
-        private void bsB_Click(object sender, EventArgs e)
-        {
+        private void bsB_Click(object sender, EventArgs e) {
             deactivateControls();
             List<int> arr = parseOrGenerate();
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
 
-            for(int i = 0; i < (arr.Count - 1); i++)
-            {
-                for(int j = 0; j < (arr.Count - 1 - i); j++)
-                {
-                    if(arr[j] > arr[j + 1])
-                    {
+            for(int i = 0; i < (arr.Count - 1); i++) {
+                for(int j = 0; j < (arr.Count - 1 - i); j++) {
+                    if(arr[j] > arr[j + 1]) {
                         swapAt(arr, j, j + 1);
                     }
                 }
@@ -111,46 +91,37 @@ namespace Sorting
             activateControls();
         }
 
-        private void inputCB_CheckedChanged(object sender, EventArgs e)
-        {
-            if(inputCB.Checked)
-            {
+        private void inputCB_CheckedChanged(object sender, EventArgs e) {
+            if(inputCB.Checked) {
                 genNUD.Enabled = false;
                 inputTB.Enabled = true;
             }
-            else
-            {
+            else {
                 genNUD.Enabled = true;
                 inputTB.Enabled = false;
             }
         }
 
-        private void swapAt(List<int> arr, int i, int j)
-        {
+        private void swapAt(List<int> arr, int i, int j) {
             int c = arr[j];
             arr[j] = arr[i];
             arr[i] = c;
         }
 
-        private void ssB_Click(object sender, EventArgs e)
-        {
+        private void ssB_Click(object sender, EventArgs e) {
             deactivateControls();
             List<int> arr = parseOrGenerate();
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
 
-            for(int i = 0; i < (arr.Count - 1); i++)
-            {
+            for(int i = 0; i < (arr.Count - 1); i++) {
                 int jMin = i, j;
-                for(j = (i + 1); j < arr.Count; j++)
-                {
-                    if(arr[j] < arr[jMin])
-                    {
+                for(j = (i + 1); j < arr.Count; j++) {
+                    if(arr[j] < arr[jMin]) {
                         jMin = j;
                     }
                 }
-                if(jMin != i)
-                {
+                if(jMin != i) {
                     swapAt(arr, i, jMin);
                 }
             }
@@ -160,19 +131,16 @@ namespace Sorting
             activateControls();
         }
 
-        private void isB_Click(object sender, EventArgs e)
-        {
+        private void isB_Click(object sender, EventArgs e) {
             deactivateControls();
             List<int> arr = parseOrGenerate();
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
 
-            for(int i = 1; i < arr.Count; i++)
-            {
+            for(int i = 1; i < arr.Count; i++) {
                 int x = arr[i];
                 int j;
-                for(j = (i - 1); (j >= 0) && (arr[j] > x); j--)
-                {
+                for(j = (i - 1); (j >= 0) && (arr[j] > x); j--) {
                     arr[j + 1] = arr[j];
                 }
                 arr[j + 1] = x;
@@ -183,43 +151,35 @@ namespace Sorting
             activateControls();
         }
 
-        private void merge(List<int> a, int i, int j, int k, List<int> b)
-        {
+        private void merge(List<int> a, int i, int j, int k, List<int> b) {
             int x = i;
             int y = j;
             int z = i;
-            while((x < j) && (y < k))
-            {
-                if(b[x] < b[y])
-                {
+            while((x < j) && (y < k)) {
+                if(b[x] < b[y]) {
                     a[z] = b[x];
                     x++;
                 }
-                else
-                {
+                else {
                     a[z] = b[y];
                     y++;
                 }
                 z++;
             }
-            while(x < j)
-            {
+            while(x < j) {
                 a[z] = b[x];
                 x++;
                 z++;
             }
-            while(y < k)
-            {
+            while(y < k) {
                 a[z] = b[y];
                 y++;
                 z++;
             }
         }
 
-        private void mergeSort(List<int> a, int i, int j, List<int> b)
-        {
-            if((j - i) > 1)
-            {
+        private void mergeSort(List<int> a, int i, int j, List<int> b) {
+            if((j - i) > 1) {
                 int k = ((i + j) / 2);
                 mergeSort(b, i, k, a);
                 mergeSort(b, k, j, a);
@@ -228,8 +188,7 @@ namespace Sorting
             }
         }
 
-        private void msB_Click(object sender, EventArgs e)
-        {
+        private void msB_Click(object sender, EventArgs e) {
             deactivateControls();
             List<int> arr = parseOrGenerate();
             Stopwatch stopwatch = new Stopwatch();
@@ -243,14 +202,35 @@ namespace Sorting
             activateControls();
         }
 
-        private void qsB_Click(object sender, EventArgs e)
-        {
+        private int partition(List<int> a, int i, int j) {
+            int pivot = a[j];
+            int x = (i - 1);
+            for(int y = i; y < j; y++) {
+                if(a[y] <= pivot) {
+                    x++;
+                    swapAt(a, x, y);
+                }
+            }
+            x++;
+            swapAt(a, x, j);
+            return x;
+        }
+
+        private void quicksort(List<int> a, int i, int j) {
+            if((j > i) && (i >= 0)) {
+                int p = partition(a, i, j);
+                quicksort(a, i, p - 1);
+                quicksort(a, p + 1, j);
+            }
+        }
+
+        private void qsB_Click(object sender, EventArgs e) {
             deactivateControls();
             List<int> arr = parseOrGenerate();
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
 
-            //sort
+            quicksort(arr, 0, arr.Count - 1);
 
             stopwatch.Stop();
             printResult(arr, stopwatch.ElapsedMilliseconds);
